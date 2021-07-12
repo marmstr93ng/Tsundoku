945971e33f7ff12d7d5f975eb13753134847d6b9
marmstr93ng <marmstr93ng@gmail.com>
2021-07-12 13:48:39 +0100

    Added logging and moved path to the base url

--- src/google_books.py
+++ src/google_books.py
@@ -1,16 +1,17 @@
 import requests
 import json
+import logging
 from typing import Dict, Any
 
 class GoogleBooks(object):
     """See: https://developers.google.com/books/docs/v1/using#WorkingVolumes"""
 
     def __init__(self):
-        self._base_url = "https://www.googleapis.com/books/v1"
+        self._base_url = "https://www.googleapis.com/books/v1/volumes"
     
     def search(self, search_term: str, max_results: int = 1) -> Dict[str, Any]:
-        path = "/volumes"
-        resp = requests.get(self._base_url+path, params={"q" : search_term, "maxResults" : max_results})
+        logging.info("Retrieving Google Books information for \"{}\"".format(search_term))
+        resp = requests.get(self._base_url, params={"q" : search_term, "maxResults" : max_results})
         if self._resp_check(resp):
             return json.loads(resp.content)
     
